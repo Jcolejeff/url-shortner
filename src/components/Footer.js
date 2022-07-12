952fb8047bd8d9aab01835de0559068e2d81646e
jcolejeff <ikwuhj@gmail.com>
2022-07-12 20:10:59 +0100

    finished siderbar

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -41,7 +41,6 @@ const Footer = () => {
 						<li> About</li>
 						<li> Our Team</li>
 
-<<<<<<< HEAD
 						<li> Careers</li>
 						<li> Contact</li>
 					</ul>
@@ -55,22 +54,6 @@ const Footer = () => {
 			</section>
 		</Wrapper>
 	);
-=======
-            <li> Careers</li>
-            <li> Contact</li>
-          </ul>
-        </div>
-        <div className="social-links">
-          <FaFacebook />
-          <FaTwitter />
-          <FaInstagram />
-          <FaPinterest />
-        </div>
-     <h6>Coded by ikwuh Jeffery<h6/>
-      </section>
-    </Wrapper>
-  );
->>>>>>> 6f29c8a2463736caa59cffecacd979ae7aff108d
 };
 
 export default Footer;
@@ -78,7 +61,6 @@ export default Footer;
 const Wrapper = styled.footer`
 	background-color: hsl(260, 8%, 14%);
 
-<<<<<<< HEAD
 	.boost {
 		display: flex;
 		flex-direction: column;
@@ -153,65 +135,4 @@ const Wrapper = styled.footer`
 			}
 		}
 	}
-=======
-  .boost {
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    color: var(--clr-white);
-    gap: 1rem;
-    background: url(${bg2}) center/cover no-repeat, hsl(257, 27%, 26%);
-    @media (min-width: 65rem) {
-      background: url(${bg}) center/cover no-repeat, hsl(257, 27%, 26%);
-    }
-    height: 30vh;
-    h3 {
-      font-size: 2rem;
-      font-weight: 500;
-    }
-    button {
-      background: hsl(180, 66%, 49%);
-      color: var(--clr-white);
-      border: none;
-      padding: 1.5rem;
-      padding-inline: 5rem;
-      border-radius: 20px;
-      font-size: 1.8rem;
-    }
-  }
-  .footer-links {
-    padding: 3rem;
-    background-color: hsl(260, 8%, 14%);
-    color: var(--clr-white);
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    gap: 4rem;
-    @media (min-width: 65rem) {
-      flex-direction: row;
-      justify-content: space-around;
-    }
-    div {
-      text-align: center;
-      font-size: 1.2rem;
-      color: hsl(257, 7%, 63%);
-    }
-    h5 {
-      font-size: 1.5rem;
-      color: var(--clr-white);
-    }
-  }
-h6{
-color: var(--clr-white);}
-  .social-links {
-    display: flex;
-    gap: 3rem;
-    svg {
-      font-size: 2.5rem;
-      color: var(--clr-white);
-    }
-  }
->>>>>>> 6f29c8a2463736caa59cffecacd979ae7aff108d
 `;
